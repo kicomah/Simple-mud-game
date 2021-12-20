beae6f1bb4d16865c30c63ddcb12388ea864d2dc
kicombo <wikibanhmi@gmail.com>
2021-12-20 21:34:06 +0900

    authorization이 없는 경우 /login으로 보내버립니다

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,14 @@ const encryptPassword = (password) => {
 }
 
 const setAuth = async (req, res, next) => {
+    // console.log(req.cookies)
     const authorization = req.cookies.authorization;
+    // authorization이 존재하지 않거나 빈 문자열이면 무조건 login으로 보내버립니다.
+    if (!authorization) {
+        return res.redirect('/login')
+    }
+    
+    // authorization이 존재하고 1자 이상의 문자열이면 key를 알아낸 뒤 user를 req에 정의합니다.
     const [bearer, key] = authorization.split(' ');
     if (bearer !== 'Bearer') {
         return res.send({error: 'Wrong Authorization'}).status(400);
