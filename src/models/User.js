0e04d2dd193704d2f55feace7a9f4d30e286f07d
kicombo <wikibanhmi@gmail.com>
2021-12-21 14:22:04 +0900

    user와 player를 하나의 개념으로 통합

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,27 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    email: {type: String, unique: true, },//이메일
-    password: String, //패스워드
-    key: String, // 인증 해쉬값
+const schema = new Schema({
+  email: {type: String, unique: true, },//이메일
+  password: String, //패스워드
+  name: String,
+  key: String, // 인증 해쉬값
+
+  level: { type: Number, default: 1 },
+  exp: { type: Number, default: 0 },
+
+  maxHP: { type: Number, default: 10 },
+  HP: { type: Number, default: 10 },
+  str: { type: Number, default: 5 },
+  def: { type: Number, default: 5 },
+  x: { type: Number, default: 0 },
+  y: { type: Number, default: 0 }
 });
+schema.methods.incrementHP = function (val) {
+  const hp = this.HP + val;
+  this.HP = Math.min(Math.max(0, hp), this.maxHP);
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", schema);
 
 module.exports = User;
--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,5 @@
 const User = require('./User')
-const Player = require('./Player')
 
 module.exports = {
-    User,
-    Player
+    User
 }
