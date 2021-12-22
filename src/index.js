8e36de1f301c99a1f5252346ddd9ca42ba2ccd01
jwp9633 <jwp9633@naver.com>
2021-12-22 14:22:54 +0900

    game.ejs 프론트 수정 사망 문구 수정

--- src/index.js
+++ src/index.js
@@ -205,7 +205,7 @@ app.post("/action", setAuth, async (req, res) => {
       event = battle(_event, player);
       // 사망 시스템
       if (player.HP <= 0) {
-        event.result = `${event.description} You die. (0,0)에서 부활합니다.`;
+        event.result = `You die. (0,0)에서 부활합니다.`;
         player.x = 0;
         player.y = 0;
         field = mapManager.getField(player.x, player.y);
